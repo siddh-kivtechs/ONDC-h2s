d10bc81f9801f0d0b0dcd3f7a06c54bb4de5ec26
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2024-03-16 23:19:23 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,19 @@
-import fs from 'fs';  
-import { logStream, logFilename } from './logging.js';  
+// import fs from 'fs';  
+// import { logStream, logFilename } from './logging.js';  
   
-export const logToFile = (ipAddress, message) => {
-  const now = new Date();
-  const formattedDate = now.toLocaleString();
-  const logMessage = `[${formattedDate}] [IP: ${ipAddress}] ${message}\n`;
-  fs.appendFile(logFilename, logMessage, (err) => {
-    if (err) {
-      console.error('Error writing to log file:', err);
-    }
-  });
-};
+// export const logToFile = (ipAddress, message) => {
+//   const now = new Date();
+//   const formattedDate = now.toLocaleString();
+//   const logMessage = `[${formattedDate}] [IP: ${ipAddress}] ${message}\n`;
+//   fs.appendFile(logFilename, logMessage, (err) => {
+//     if (err) {
+//       console.error('Error writing to log file:', err);
+//     }
+//   });
+// };
 
-export const logMessage = (message) => {
-  const now = new Date();
-  const formattedDate = now.toLocaleString();
-  logStream.write(`[${formattedDate}] ${message}\n`);
-};
+// export const logMessage = (message) => {
+//   const now = new Date();
+//   const formattedDate = now.toLocaleString();
+//   logStream.write(`[${formattedDate}] ${message}\n`);
+// };
