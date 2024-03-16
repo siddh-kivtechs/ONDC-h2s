143eded60fc0eb40de83998992f187403cad74c4
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2024-03-17 00:42:11 +0530

    Update ejsHandler.js

--- src/ejsHandler.js
+++ src/ejsHandler.js
@@ -1,24 +1,19 @@
-import express from 'express';    
-  
-const router = express.Router();    
-  
-// Add a route to show the EJS file    
-router.get('/', (req, res) => {  
-  let latitude = req.headers['x-vercel-ip-latitude']||23;  
-  let longitude = req.headers['x-vercel-ip-longitude']||85;  
-  let location = req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country']||'DEL,DEL,INDIA';  
-  let ip_address = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;  
-  
-  const data = {  
-    latitude: latitude,  
-    longitude: longitude,  
-    location: location,  
-    ip_address: ip_address,  
-  };  
-    
-  res.render('main', data);  
-});  
+import express from 'express';
 
-  
-    
-export default router;    
+const handler = (req, res) => {
+  const latitude = req.headers['x-vercel-ip-latitude'];
+  const longitude = req.headers['x-vercel-ip-longitude'];
+  const location = req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country'];
+  const ip_address = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  const data = {
+    latitude,
+    longitude,
+    location,
+    ip_address,
+  };
+
+  res.render('main', data);
+};
+
+export default handler;
