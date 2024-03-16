87e5e95fcb4ac9abf3da2b4d3acefbd95850d7f6
siddht1 <rt1yrru+github@gmail.com>
2024-03-16 23:15:36 +0530

    ab

--- src/logging.js
+++ src/logging.js
@@ -1,8 +1,21 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+// import fs from 'fs';
 
-const logFilename = fileURLToPath(new URL('./logs.txt', import.meta.url));
-const logStream = fs.createWriteStream(path.resolve(logFilename), { flags: 'a' });
+// const logFilename = fileURLToPath(new URL('./logs.txt', import.meta.url));
+// const logStream = fs.createWriteStream(path.resolve(logFilename), { flags: 'a' });
 
-export { logStream, logFilename };
+// export { logStream, logFilename };
+
+//  for vercel 
+import { Console } from 'console'; // Import the Console class
+
+// Create a custom console object with filtering (optional)
+const customConsole = new Console({
+  stdout: process.stdout, // Redirect output to standard output
+  stderr: process.stderr, // Redirect errors to standard error
+  // You can add filtering logic here if needed
+  // (e.g., only log messages with severity level 'error')
+});
+
+export { customConsole }; // Export the custom console for logging
